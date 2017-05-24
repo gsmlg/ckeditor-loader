df1d5bce217971e7ed4cf127ce70c5170c2e91c5
gaoshiming <gaoshiming@zdns.cn>
2017-05-24 13:45:26 +0800

    make loader works

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # ckeditor-loader
 webpack loader to load ckeditor
+
+
+## USAGE
+
+code:
+```javascript
+import $ from 'jquery';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import Editor from './ckeditor.conf';
+
+window.$ = $;
+window.editor = Editor;
+
+class App extends Component {
+
+  componentDidMount() {
+    var context = this.refs.editorContext;
+    this.editor = Editor.appendTo(context, {
+      customConfig: '',
+      width: 640,
+      height: 480,
+    });
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <header>
+          <h1>CKEditor</h1>
+        </header>
+        <section>
+          <div ref="editorContext" />
+        </section>
+      </div>
+    );
+  }
+}
+
+$(function() {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app'),
+    () => {
+      console.log('App mounted!');
+    }
+  );
+});
+
+
+```
+
+editor.conf:
+```javascript
+module.exports = {
+    // TODO: support config ckeditor
+};
+```
+
+loader:
+```javascript
+module: {
+    rules: [
+    ...
+    {
+        test: /ckeditor.conf$/,
+        use: [
+            'ckeditor-loader'
+        ]
+    }
+    ]
+```
--- index.js
+++ index.js
@@ -21,26 +21,46 @@ module.exports = function(conf) {
   let ckb = fs.readFileSync(ckf);
   var ck = ckb.toString('utf8');
 
-  var langf = path.join(ckf, '../lang/zh-cn.js');
-  var lang = fs.readFileSync(langf).toString('utf8').replace(/^\uFEFF/, '');
-
-  var stylef = path.join(ckf, '../styles.js');
-  var styles = fs.readFileSync(stylef).toString('utf8').replace(/^\uFEFF/, '');
+  console.log(arguments);
 
   ck = ck.replace(/^\uFEFF/, '');
   ck = ck.replace(/window\.CKEDITOR/g, 'CKEDITOR');
   ck = ck.replace(/window.CKEDITOR_BASEPATH/g, 'CKEDITOR_BASEPATH');
+
   var code = '';
   code += 'var CKEDITOR;';
   code += 'var CKEDITOR_BASEPATH = "";';
   code += 'var CKEDITOR_GETURL;';
   code += ck;
-  code += lang;
-  code += styles;
+
+  code += readFile('config.js');
+  code += readFile('lang/zh-cn.js');
+  code += readFile('styles.js');
+  code += readFile('plugins/clipboard/dialogs/paste.js');
+  code += readFile('plugins/table/dialogs/table.js');
+  code += readFile('plugins/about/dialogs/about.js');
+  code += readFile('plugins/link/dialogs/link.js');
+  code += readFile('plugins/link/dialogs/anchor.js');
+  code += readFile('plugins/image/dialogs/image.js');
+  code += readFile('plugins/specialchar/dialogs/lang/zh-cn.js');
+  code += readFile('plugins/specialchar/dialogs/specialchar.js');
+  code += readFile('plugins/scayt/dialogs/options.js');
+
+  code += 'CKEDITOR.dom.document.prototype.appendStyleSheet = console.log;';
+  code += 'CKEDITOR.config.contentsCss = null;';
+
   code += '\nwindow.CKEDITOR = CKEDITOR;\nmodule.exports = CKEDITOR;';
+
   code += 'require("ckeditor/skins/moono/editor.css");';
-  code += 'require("ckeditor/contents.css");';
   code += 'require("ckeditor/plugins/scayt/skins/moono-lisa/scayt.css");';
   code += 'require("ckeditor/plugins/wsc/skins/moono-lisa/wsc.css");';
+  code += 'require("ckeditor/contents.css");';
+  code += 'require("ckeditor/skins/moono-lisa/dialog.css");';
+  return code;
+};
+
+var readFile = function(name) {
+  var file = path.join(ckf, '..', name);
+  var code = fs.readFileSync(file).toString('utf8').replace(/^\uFEFF/, '');
   return code;
 };
